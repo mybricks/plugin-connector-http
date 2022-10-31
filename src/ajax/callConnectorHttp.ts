e463b14e6934844e24c258246312cff556eed341
yankewen <yankewen@kuaishou.com>
2022-10-31 10:32:27 +0800

    feat: add callConnectorHttp

---
+++ src/ajax/callConnectorHttp.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+interface IOptions {
+  method: string;
+  url: string;
+  data: any;
+  params: any;
+  headers: any;
+  [key: string]: any;
+}
+
+interface IConfig {
+  beforeSend: (options: IOptions) => any;
+}
+
+const defaultFn = (options: IOptions, ...args: any) => ({
+  ...options,
+  ...args,
+});
+
+export function callConnectorHttp(
+  connector: { id: string; script: string; [key: string]: any },
+  params: any,
+  config?: IConfig
+) {
+  return new Promise((resolve, reject) => {
+    try {
+      const fn = eval(`(${decodeURIComponent(connector.script)})`);
+      const { beforeSend = defaultFn } = config || {};
+      fn(
+        params,
+        { then: resolve, onError: reject },
+        {
+          ajax(options: IOptions) {
+            const opts = beforeSend({ ...options });
+            return axios(opts || options).then((res: any) => res.data);
+          },
+        }
+      );
+    } catch (ex) {
+      reject(`连接器script错误.`);
+    }
+  });
+}
