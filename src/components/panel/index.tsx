2f6c61e9633f21c7aa1e8379dda87c6cb28fc3cb
liulei11 <liulei11@kuaishou.com>
2023-06-27 11:56:42 +0800

    feat: 插件面板优先级

--- src/components/panel/index.tsx
+++ src/components/panel/index.tsx
@@ -1,14 +1,13 @@
 import React, {FC, useEffect} from 'react';
 import {render, unmountComponentAtNode} from "react-dom";
 
-let container = null;
+let container = document.querySelector('[data-id=http-plugin-panel-root]');
+if (!container) {
+	container = document.createElement('div');
+	container.setAttribute('data-id', 'http-plugin-panel-root');
+	document.body.appendChild(container);
+}
 const PanelWrap: FC = props => {
-	if (!container) {
-		container = document.createElement('div');
-		container.setAttribute('data-id', 'plugin-panel-root');
-		document.body.appendChild(container);
-	}
-	
 	useEffect(() => {
 		render(<>{props.children}</>, container);
 		
