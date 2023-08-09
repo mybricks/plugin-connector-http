d8e6b4f948b738a805a388def929f987b0c022de
liulei11 <liulei11@kuaishou.com>
2023-08-09 14:39:10 +0800

    feat: css 类名打包

--- scripts/webpack.common.js
+++ scripts/webpack.common.js
@@ -45,7 +45,9 @@
           {
             loader: 'css-loader',
             options: {
-              modules: true,
+              modules: {
+                localIdentName: '[local]_[hash:base64:5]',
+              },
             },
           },
           {
