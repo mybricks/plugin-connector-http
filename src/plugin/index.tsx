2b5fc264f236f757c5b4266f8d8e449d7f0b33cb
Leo <liulei.leo306@gmail.com>
2023-09-11 14:38:36 +0800

    feat: 代码 format

--- src/plugin/index.tsx
+++ src/plugin/index.tsx
@@ -114,8 +114,8 @@ export default function Sidebar({
 				  type:
 					  sidebarContext.formModel.type || sidebarContext.type || 'http',
 				  title: others.title,
-					connectorName: PLUGIN_CONNECTOR_NAME,
-					globalMock: data.config.globalMock,
+                  connectorName: PLUGIN_CONNECTOR_NAME,
+                  globalMock: data.config.globalMock,
 				  inputSchema: others.inputSchema,
 				  outputSchema: others.outputSchema,
 			  });
@@ -137,8 +137,8 @@ export default function Sidebar({
 							  id: updateAll ? serviceItem.id : id,
 							  title: others.title || serviceItem.content.title,
 							  type: service.type,
-								connectorName: PLUGIN_CONNECTOR_NAME,
-								globalMock: data.config.globalMock,
+                              connectorName: PLUGIN_CONNECTOR_NAME,
+                              globalMock: data.config.globalMock,
 							  inputSchema: serviceItem.content.inputSchema,
 							  outputSchema: serviceItem.content.outputSchema,
 						  });
