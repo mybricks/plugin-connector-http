6429bc19ee4b3e068fc5093a433ba14e3a63835b
Leo <liulei.leo306@gmail.com>
2023-09-13 19:04:55 +0800

    feat: 连接器修复问题：导入后调试找不到script

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import Plugin from './plugin';
 import { icon } from './icon';
 import data from './data';
-import GlobalContext, { getPureConnectors } from './context';
 import { exampleParamsFunc, PLUGIN_CONNECTOR_NAME, templateResultFunc } from './constant';
 import { call } from './runtime/callConnectorHttp';
 import { getScript, getDecodeString } from './script';
@@ -20,29 +19,26 @@ export default function pluginEntry(pluginConfig: any = {}) {
     title: '连接器',
     description: '连接器',
     data,
-    onLoad: ({ data }) => {
-      if (!data) {
+    onLoad() {
+      if (!this.data) {
         return;
       }
 
       /** 初始化全局配置 */
-      data.config = data.config || {
+      this.data.config = this.data.config || {
         paramsFn: pluginConfig?.initialValue?.paramsFn || exampleParamsFunc,
         resultFn: pluginConfig?.initialValue?.resultFn || templateResultFunc,
       };
-      data.config.resultFn = data.config.resultFn || pluginConfig?.initialValue?.resultFn || templateResultFunc;
-
-      /** PureConnectors 保存 data 的索引地址，后续无需重复更新 PureConnectors */
-      GlobalContext.initPureConnectors(data);
+      this.data.config.resultFn = this.data.config.resultFn || pluginConfig?.initialValue?.resultFn || templateResultFunc;
     },
     /** 调试时将调用插件的 callConnector 方法 */
-    callConnector: (connector, params, config) => {
+    callConnector(connector, params, config) {
       /** 启动 Mock */
       if (config?.openMock) {
         return mock({ ...connector, outputSchema: config.mockSchema });
       }
 
-      const pureConnectors = { ...getPureConnectors() };
+      const pureConnectors = { ...this.data };
       /** mode = test，即在编辑面板点击调试 */
       const findConnector = connector.mode === 'test' ? connector : pureConnectors.connectors.find(con => con.id === connector.id);
       if (findConnector) {
@@ -63,8 +59,8 @@ export default function pluginEntry(pluginConfig: any = {}) {
       }
     },
     /** 页面导出 JSON 时，会调用插件 toJSON 方法，数据防止在页面 JSON 中 */
-    toJSON: () => {
-      const pureConnectors = { ...getPureConnectors() };
+    toJSON({ data }) {
+      const pureConnectors = { ...data };
 
       if (pluginConfig?.pure) {
         try {
