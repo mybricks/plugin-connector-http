896d343108ce7b9bbc9caf616b2902c47c81c305
Leo <liulei.leo306@gmail.com>
2023-11-24 16:39:11 +0800

    feat: 兼容 callConnector 默认携带 script

--- src/index.tsx
+++ src/index.tsx
@@ -41,7 +41,7 @@ export default function pluginEntry(pluginConfig: any = {}) {
       }
 
       /** mode = test，即在编辑面板点击调试 */
-      const findConnector = connector.mode === 'test' ? connector : pureConnectors.connectors.find(con => con.id === connector.id);
+      const findConnector = connector.mode === 'test' ? connector : (pureConnectors.connectors.find(con => con.id === connector.id) ?? (connector.script ? connector : null));
       if (findConnector) {
         let curConnector = { ...findConnector };
         /**  支持 json 方式运行 */
