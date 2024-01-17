2efe1a9b9aace404010356c58f9b80e8c0014e8c
zhanlin <zhanlin@kuaishou.com>
2024-01-17 14:19:28 +0800

    feat: 极速版

--- src/plugin/index.tsx
+++ src/plugin/index.tsx
@@ -10,6 +10,8 @@ export default function ({ command }) {
   const [success, setSuccess] = useState(false);
   const [intentCost, setIntentCost] = useState(null);
   const [generateCost, setGenerateCost] = useState(null);
+  const [isMultiMode, setIsMultiMode] = useState(false);
+  const [simpleModeCost, setSimpleModeCost] = useState(null);
 
   const handleRequirementChange = useCallback((e) => {
     setRequirement(e.target.value);
@@ -22,62 +24,110 @@ export default function ({ command }) {
     setError(false);
     setIntentCost(null);
     setGenerateCost(null);
-    axios
-      .post("http://127.0.0.1:13000/api/ai/intent-conjecture2", {
-        // .post("https://ai.mybricks.world/api/ai/intent-conjecture2", {
-        demand: requirement,
-      })
-      .then(async (res) => {
-        if (res.data.code === 1) {
-          console.log("---", res.data);
-          const schema = res.data.data.result;
-          const comArray = schema.map((item) => ({
-            type: item.namespace,
-            data: item.data,
-            slots: item.slots
-              ? {
-                  content: item.slots.map((slotsItem) => ({
-                    type: slotsItem.namespace,
-                    data: slotsItem.data,
-                  })),
-                }
-              : undefined,
-          }));
+    setSimpleModeCost(null);
+    if (isMultiMode) {
+      axios
+        .post("https://ai.mybricks.world/api/ai/intent-conjecture2", {
+          // .post("https://ai.mybricks.world/api/ai/intent-conjecture2", {
+          demand: requirement,
+        })
+        .then(async (res) => {
+          if (res.data.code === 1) {
+            console.log("---", res.data);
+            const schema = res.data.data.result;
+            const comArray = schema.map((item) => ({
+              type: item.namespace,
+              data: item.data,
+              slots: item.slots
+                ? {
+                    content: item.slots.map((slotsItem) => ({
+                      type: slotsItem.namespace,
+                      data: slotsItem.data,
+                    })),
+                  }
+                : undefined,
+            }));
 
-          const component = { data: comArray };
-          console.log("AI res: ", component);
-          const execRes = await command.exec("ui.addComs", component);
-          console.log("exec res: ", execRes);
+            const component = { data: comArray };
+            console.log("AI res: ", component);
+            const execRes = await command.exec("ui.addComs", component);
+            console.log("exec res: ", execRes);
 
-          setIntentCost(res.data.data.intentCost);
-          let unitOperationTotalTokens = 0;
-          res.data.data.logs.forEach((item) => {
-            unitOperationTotalTokens += item.cost.generate.usage.total_tokens;
-          });
-          let unitOperationMaxTime = Math.max(
-            ...res.data.data.logs.map(
-              (item) => item.cost.vec.time + item.cost.generate.time
-            )
-          );
-          setGenerateCost({
-            unitOperationTotalTokens,
-            unitOperationMaxTime,
-          });
-        }
-        setSuccess(true);
-        setRequirement("");
-      })
-      .catch((err) => {
-        console.log(err);
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [loading, requirement]);
+            setIntentCost(res.data.data.intentCost);
+            let unitOperationTotalTokens = 0;
+            res.data.data.logs.forEach((item) => {
+              unitOperationTotalTokens += item.cost.generate.usage.total_tokens;
+            });
+            let unitOperationMaxTime = Math.max(
+              ...res.data.data.logs.map(
+                (item) => item.cost.vec.time + item.cost.generate.time
+              )
+            );
+            setGenerateCost({
+              unitOperationTotalTokens,
+              unitOperationMaxTime,
+            });
+            setSuccess(true);
+            setRequirement("");
+          } else {
+            setError(true);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          setError(true);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    } else {
+      axios
+        .post("https://ai.mybricks.world/api/ai/addUnitSchema", {
+          // .post("https://ai.mybricks.world/api/ai/intent-conjecture2", {
+          requirement,
+        })
+        .then(async (res) => {
+          if (res.data.code === 1) {
+            console.log("---", res.data);
+            const schema = res.data.data.unitSchema;
+            const component = {
+              data: { type: schema.namespace, data: schema.data },
+            };
+            console.log("AI res: ", component);
+            const execRes = await command.exec("ui.addComs", component);
+            console.log("exec res: ", execRes);
+            setSimpleModeCost(res.data.data.cost);
+            setSuccess(true);
+            setRequirement("");
+          } else {
+            setError(true);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          setError(true);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [loading, requirement, isMultiMode]);
 
   return (
     <div className={css.container}>
+      <div className={css.mode}>
+        <label className={css.label}>多组件模式：</label>
+        <label className={css.switch}>
+          <input
+            type="checkbox"
+            checked={isMultiMode}
+            onChange={(e) => {
+              setIsMultiMode(e.target.checked);
+            }}
+          ></input>
+          <span className={css.slider}></span>
+        </label>
+      </div>
       <textarea
         className={css.input}
         value={requirement}
@@ -124,15 +174,51 @@ export default function ({ command }) {
             </div>
           </li>
         )}
+        {simpleModeCost && (
+          <>
+            <li>
+              <span>向量检索：</span>
+              <div className={css.time}>{simpleModeCost.vec.time / 1000}s</div>
+            </li>
+            <li>
+              <span>组件生成：</span>
+              <div className={css.time}>
+                {simpleModeCost.generate.time / 1000}s
+              </div>
+              <div className={css.token}>
+                {simpleModeCost.generate.usage.total_tokens} tokens
+              </div>
+            </li>
+            <li>
+              <span>总和：</span>
+              <div className={css.time}>
+                {(simpleModeCost.vec.time + simpleModeCost.generate.time) /
+                  1000} s
+              </div>
+              <div className={css.token}>
+                {simpleModeCost.generate.usage.total_tokens} tokens
+              </div>
+            </li>
+          </>
+        )}
       </ul>
-      <div style={{ fontSize: 12, color: "#777",marginTop:'10px',userSelect:'text' }}>
+      <div
+        style={{
+          fontSize: 12,
+          color: "#777",
+          marginTop: "10px",
+          userSelect: "text",
+        }}
+      >
         示例问题：
         <p>添加一个按钮 一个文本框 一个日历</p>
-        <p>添加一个按钮，风格是主按钮，小尺寸，标题是添加活动</p>
-        <p>添加一个按钮，风格是危险，大尺寸，标题是删除</p>
+        <p>添加一个危险按钮，风格是次按钮，小尺寸，标题是添加活动</p>
+        <p>添加一个圆角虚线按钮，大尺寸，标题是删除</p>
         <p>添加一个三列表格，分别是姓名、年龄、学号</p>
         <p>下拉菜单，触发方式为点击</p>
-        <p>添加一个下拉菜单，弹出位置是右下方，提示内容设置为请选择，宽度设置为120px，并关闭子选项</p>
+        <p>
+          添加一个下拉菜单，弹出位置是右下方，提示内容设置为请选择，宽度设置为120px，并关闭子选项
+        </p>
         <p>
           添加一个多选框组件，名称是兴趣爱好，选项有足球、篮球、羽毛球、排球，最大支持选三样
         </p>
